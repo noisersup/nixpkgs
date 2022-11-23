09725920d45eea3494f580150566216f5fd600df
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2022-11-23 20:58:42 +0400

    ferretdb: refactor
    Tweak the build process to set correct version.

--- pkgs/servers/nosql/ferretdb/default.nix
+++ pkgs/servers/nosql/ferretdb/default.nix
@@ -5,28 +5,31 @@
 
 buildGoModule rec {
   pname = "ferretdb";
-  version = "0.7.1";
+  version = "0.8.0";
 
   src = fetchFromGitHub {
     owner = "FerretDB";
     repo = "FerretDB";
     rev = "v${version}";
-    sha256 = "sha256-i3XCYVJfZ2sF4XGOxaBZqBOw7nRdzcGKhNNdqQMccPU=";
+    sha256 = "sha256-g4UJOgT/87RDRqNU5Zp7/47oPLtRNjYJT687PGd3KwE=";
   };
 
   postPatch = ''
-    echo ${version} > internal/util/version/gen/version.txt
+    echo v${version} > build/version/version.txt
+    echo nixpkgs     > build/version/package.txt
   '';
 
-  vendorSha256 = "sha256-qyAc5EVg8QPTnXQjqJGpT3waDrfn8iXz+O1iESCzCIc=";
+  vendorSha256 = "sha256-VWDRwf3pnD+Q516d05kfm0iPUuSHs00QKGYN4rqNfw4=";
 
   CGO_ENABLED = 0;
 
   subPackages = [ "cmd/ferretdb" ];
 
+  tags = [ "ferretdb_tigris" ];
+
   meta = with lib; {
     description = "A truly Open Source MongoDB alternative";
-    homepage = "https://github.com/FerretDB/FerretDB";
+    homepage = "https://www.ferretdb.io/";
     license = licenses.asl20;
     maintainers = with maintainers; [ dit7ya ];
   };
