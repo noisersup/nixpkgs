b68ce5f42f8411748955716f24672149900a1672
noisersup <patryk@kwiatek.xyz>
2023-01-16 18:16:19 +0100

    ferretdb: 0.8.0 -> 0.8.1

--- pkgs/servers/nosql/ferretdb/default.nix
+++ pkgs/servers/nosql/ferretdb/default.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "ferretdb";
-  version = "0.8.0";
+  version = "0.8.1";
 
   src = fetchFromGitHub {
     owner = "FerretDB";
     repo = "FerretDB";
     rev = "v${version}";
-    sha256 = "sha256-g4UJOgT/87RDRqNU5Zp7/47oPLtRNjYJT687PGd3KwE=";
+    sha256 = "sha256-R2B1UlnNay9McnBIbwFMw4g8TFWZoQH4xzJCvdcJUZo=";
   };
 
   postPatch = ''
@@ -19,7 +19,7 @@ buildGoModule rec {
     echo nixpkgs     > build/version/package.txt
   '';
 
-  vendorSha256 = "sha256-VWDRwf3pnD+Q516d05kfm0iPUuSHs00QKGYN4rqNfw4=";
+  vendorSha256 = "sha256-NHuv0aM4XULJxzSX6uWuZZu0s9AdWjPLvjDJzaaAzDk=";
 
   CGO_ENABLED = 0;
 
