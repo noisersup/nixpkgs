fad0dacc382798d49d14ba6dbfddc5891ef89e71
noisersup <patryk@kwiatek.xyz>
2023-08-07 12:41:56 +0200

    wip

--- pkgs/applications/misc/BambuStudio/default.nix
+++ pkgs/applications/misc/BambuStudio/default.nix
@@ -1,9 +1,19 @@
-{ stdenv
+{ lib
+, stdenv
 , fetchFromGitHub
 , cmake
 , pkg-config
+, mesa
+, dbus
+, boost
+, git
+, libGL
+, freetype
+, libGLU, cairo, gtk3, libsoup,  openvdb, wayland, wayland-protocols, libxkbcommon
+, tbb, openssl, curl, glew, glfw, cereal, nlopt, ilmbase, cgal, opencascade-occt
+, wxGTK31
 }:
-stdenv.mkDerivation rec {
+let
   pname = "BambuStudio";
   version = "01.07.01.62";
 
@@ -14,5 +24,45 @@ stdenv.mkDerivation rec {
     sha256 = "lkB4OzK4UyQwyKHX76xfGmN83Riu/YQRQlXzJPLdOUU=";
   };
 
-  nativeBuildInputs = [ cmake pkg-config ];
+  bambuSrc = src;
+
+  wxGTK-patched = wxGTK31.overrideAttrs (old: rec {
+    patches = old.patches ++ [
+      ("${bambuSrc}/deps/wxWidgets/0001-wxWidget-fix.patch")
+    ];
+
+    src = fetchFromGitHub {
+      owner = "wxWidgets";
+      repo = "wxWidgets";
+      rev = "v3.1.5";
+      hash = "sha256-2zMvcva0GUDmSYK0Wk3/2Y6R3F7MgdqGBrOhmWgVA6g=";
+      fetchSubmodules = true;
+    };
+  });
+in stdenv.mkDerivation {
+  inherit pname version src;
+
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-DDEP_WX_GTK3=1"
+    "-DwxWidgets_INCLUDE_DIRS=${wxGTK-patched}"
+    "-DwxWidgets_LIBRARIES=${wxGTK-patched}/lib"
+    "-DwxWidgets_ROOT_DIR:PATH=${wxGTK-patched}"
+  ];
+
+
+  # nativeBuildInputs = [ cmake pkg-config gcc mesa dbus boost git libGL freetype
+  # libGLU cairo gtk3 libsoup openvdb wayland wayland-protocols libxkbcommon
+  # tbb openssl curl glew glfw cereal nlopt ilmbase cgal opencascade-occt
+  # ];
+
+  buildInputs = [
+  cmake pkg-config mesa dbus boost git libGL freetype
+  libGLU cairo gtk3 libsoup openvdb wayland wayland-protocols libxkbcommon
+  tbb openssl curl glew glfw cereal nlopt ilmbase cgal opencascade-occt
+  wxGTK-patched
+  ];
+  #buildInputs = [
+  #  wxGTK-patched
+  #];
 }
